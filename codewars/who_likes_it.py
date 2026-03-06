c99381759260d34a0449b07d7e7433b297245032
peeb. <106175736+MarlGian@users.noreply.github.com>
2026-03-06 21:58:00 +0800

    Implement likes function to handle name lists

---
+++ codewars/who_likes_it.py
@@ -0,0 +1,16 @@
+def likes(names):
+    if not names:
+        return "no one likes this"
+    if len(names) < 2:
+        return f"{names[0]} likes this"
+    if len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    if len(names) > 2 and len(names) <= 3:
+        return  f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+print(likes([]))
+print(likes(["Peter"]))
+print(likes(["Max", "John", "Mark"]))
+print(likes(["Alex", "Jacob", "Mark", "Max"]))
